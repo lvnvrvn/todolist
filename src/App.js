7816006b5c3725bd0e30a3b61bca39ec6072f492
Olga <lvnvrvn@yandex.ru>
2023-07-12 13:11:04 +0300

    updated localstorage

--- src/App.js
+++ src/App.js
@@ -3,38 +3,76 @@ import { useState, useEffect, useRef } from 'react';
 import Item from './Item';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      title: 'купить хлеб',
-      isDone: true
-    },
-    {
-      id: 2,
-      title: 'посмотреть фильм',
-      isDone: false
-    },
-    {
-      id: 3,
-      title: 'почитать журнал',
-      isDone: true
-    }
-  ]);
+  const [items, setItems] = useState([]);
 
   const input = useRef(null);
 
-  function addItem() {
-    setItems([...items, {
-      id: items.length + 1,
+  function addItemsFromLS() {
+    const storageItems = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        storageItems.push(JSON.parse(localStorage.getItem(localStorage.key(i)))); // в фунцкию отдельную вынести
+      }
+    setItems(storageItems);
+  }
+
+  useEffect(() => {
+    // items.forEach(item => {
+    //   localStorage.setItem(item.id, JSON.stringify(item))
+    // });
+    
+    if (localStorage.length > 0) {
+      addItemsFromLS();
+    } else {
+      setItems([
+        {
+          id: 1,
+          title: 'купить хлеб',
+          isDone: true
+        },
+        {
+          id: 2,
+          title: 'посмотреть фильм',
+          isDone: false
+        },
+        {
+          id: 3,
+          title: 'почитать журнал',
+          isDone: true
+        }
+      ]);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   const lastItem = items[items.length - 1];
+  //   localStorage.setItem(lastItem.id, JSON.stringify(lastItem));
+  // });
+
+  useEffect(() => {
+    addItemsFromLS();  
+  }, [localStorage.length]);
+
+  function addItem() { //заменить setitem на добавление нового item в localstorage
+    // setItems([...items, {
+    //   id: items.length + 1,
+    //   title: input.current.value,
+    //   isDone: false
+    // }]);
+    // console.log(items);
+
+    localStorage.setItem(localStorage.length + 1, {
+      id: localStorage.length + 1,
       title: input.current.value,
       isDone: false
-    }]);
+    });
+    
     input.current.value = '';
   }
 
   function removeItem(id) {
-    const newItems = items.filter(item => item.id !== id);
-    setItems(newItems);
+    localStorage.removeItem(id);
+    // const newItems = items.filter(item => item.id !== id);
+    // setItems(newItems);
   }
 
   function clear() {
@@ -48,7 +86,8 @@ function App() {
   // }
 
   function onEnter(event) {
-    if (event.keyCode == 13) {
+    if (event.keyCode === 13) {
+      
       addItem();
       console.log(items);
     }
@@ -56,7 +95,7 @@ function App() {
 
   return (
     <div className="App">
-      <form className='form' action="/" onKeyDown={(e) => {if (e.keyCode == 13) return false;}}>
+      <form className='form' action="/" onSubmit={(e) => e.preventDefault()}>
         <input className='input' type="text" ref={input} onKeyDown={onEnter} />
         <button className='delete' type='button' onClick={clear}>&#10006;</button>
         <button className='add' type='button' onClick={addItem}>добавить</button>
